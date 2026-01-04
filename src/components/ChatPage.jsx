004b9da99201bafb09d295da12c7ceeed337ef4b
Harsh Patil <itsharshhh6@gmail.com>
2026-01-04 11:41:56 +0530

    Designed the static part

--- src/components/ChatPage.jsx
+++ src/components/ChatPage.jsx
@@ -1,14 +1,57 @@
 import React from 'react'
+import { MdAttachFile, MdSend } from 'react-icons/md'
 
 export const ChatPage = () => {
   return (
-    <div>
-        <header>
-            <div><h1>Room : <span>Family Group</span></h1></div>
-            <div></div>
-            <div></div>
-        </header>
+    <div className="">
+      {/* this is a header */}
+      <header className="dark:border-gray-700  fixed w-full dark:bg-gray-900 py-5 shadow flex justify-around items-center">
+        {/* room name container */}
+        <div>
+          <h1 className="text-xl font-semibold">
+            Room : <span>Family Group</span>
+          </h1>
+        </div>
+        {/* username container */}
+
+        <div>
+          <h1 className="text-xl font-semibold">
+            User : <span>Harsh</span>
+          </h1>
+        </div>
+        {/* button: leave room */}
+        <div>
+          <button
+            className="dark:bg-red-500 dark:hover:bg-red-700 px-3 py-2 rounded-full"
+          >
+            Leave Room
+          </button>
+        </div>
+      </header>
+
+      
+      {/* input message container */}
+      <div className=" fixed bottom-4 w-full h-16 ">
+        <div className="h-full  pr-10 gap-4 flex items-center justify-between rounded-full w-1/2 mx-auto dark:bg-gray-900">
+          <input            
+            type="text"
+            placeholder="Type your message here..."
+            className=" w-full  dark:border-gray-600 b dark:bg-gray-800  px-5 py-2 rounded-full h-full focus:outline-none  "
+          />
+
+          <div className="flex gap-1">
+            <button className="dark:bg-purple-600 h-10 w-10  flex   justify-center items-center rounded-full">
+              <MdAttachFile size={20} />
+            </button>
+            <button
+              className="dark:bg-green-600 h-10 w-10  flex   justify-center items-center rounded-full"
+            >
+              <MdSend size={20} />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
