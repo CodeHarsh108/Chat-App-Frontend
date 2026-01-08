4c73562800cb35d54b44a4522e312b2aa9e5e016
Harsh Patil <itsharshhh6@gmail.com>
2026-01-08 18:20:51 +0530

    Form Handling

--- src/components/JoinCreateChat.jsx
+++ src/components/JoinCreateChat.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import chatIcon from '../assets/chat.png'
+import { MdRoom } from 'react-icons/md';
+import toast from 'react-hot-toast';
 
 
 const JoinCreateChat = () => {
+
+
+  const [details, setDetails] = useState({
+    roomId : '',
+    username : ''
+  });
+
+
+
+
+  function handleFormInputChange(event){
+    setDetails({
+      ...details,
+      [event.target.name] : event.target.value,
+    })
+  }
+
+
+  function validateForm(){
+    if(details.username.trim() === '' || details.roomId.trim() === ''){
+      toast.error("Please fill all the fields");
+      return false;
+    }
+  }
+
+  function joinChat(){
+    if(!validateForm()){
+      return;
+    }
+    // proceed to join the chat
+    alert(`Joining room ${details.roomId} as ${details.username}`);
+  }
+
+  function createRoom(){
+
+  }
+
   return (
     <div className='min-h-screen flex items-center justify-center'>
 
@@ -14,18 +53,18 @@ const JoinCreateChat = () => {
             <h1 className='text-2xl font-semibold text-center margin'>Join Room Or Create Room</h1>
             <div className=''>
                 <label htmlFor="name" className='block font-medium mb-2'>Your Name : </label>
-                <input type="text" id='name' className='w-full dark:bg-gray-200 px-4 py-2 border dark:border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black'/>
+                <input onChange={handleFormInputChange} value={details.username} name='username' placeholder='Enter your name ' type="text" id='name' className='w-full dark:bg-gray-200 px-4 py-2 border dark:border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black'/>
             </div>
 
 
             <div className=''>
                 <label htmlFor="name" className='block font-medium mb-2'>Room Id : </label>
-                <input type="text" id='name' className='w-full dark:bg-gray-200 px-4 py-2 border dark:border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black'/>
+                <input onChange={handleFormInputChange} value={details.roomId} name='roomId' placeholder='Enter room id ' type="text" id='name' className='w-full dark:bg-gray-200 px-4 py-2 border dark:border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black'/>
             </div>
 
             <div>
-                <button className='w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg transition duration-300'>Join Room</button>
-                <button className='w-full mt-3 bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded-lg transition duration-300'>Create  Room</button>
+                <button onClick={joinChat} className='w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg transition duration-300'>Join Room</button>
+                <button onClick={createRoom} className='w-full mt-3 bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded-lg transition duration-300'>Create  Room</button>
             </div>
 
 
